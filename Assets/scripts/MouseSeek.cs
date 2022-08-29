ad6da1fd303b44f147e36125881cb69ce8567ec2
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2022-08-29 11:47:06 -0500

    Trabajo funcional

--- Assets/scripts/MouseSeek.cs
+++ Assets/scripts/MouseSeek.cs
@@ -1,37 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseSeek : MonoBehaviour
 {
     private Vector3 postion; //distance
     public float maxV;
-    public Vector3 steering;
-    public Vector3 finalV;
-
-    public Vector3 vDisired;
-    public Vector3 Velocity;
+    private Vector3 vDisired;
+    private Vector3 steering;
+    private Vector3 finalV; 
 
     void Update()
     {
-        /*
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         Vector2 direction = mousePosition - transform.position;
         float angle = Vector2.SignedAngle(Vector2.right, direction);
         transform.eulerAngles = new Vector3(0, 0, angle);
-        */
 
         postion = transform.position;
 
         postion = Camera.main.ScreenToWorldPoint(Input.mousePosition) - postion;
 
-        vDisired = postion.normalized * maxV * Time.deltaTime;
+        vDisired = postion.normalized * maxV;
 
-        steering = vDisired + Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        steering = vDisired * Time.deltaTime;
 
-        finalV = steering * maxV * Time.deltaTime;
+        finalV = steering;
 
-        transform.position += finalV;
+        transform.position += new Vector3(finalV.x, finalV.y, 0);
     }
 }

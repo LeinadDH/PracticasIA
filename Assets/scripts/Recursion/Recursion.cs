dc918213d2818bf089f310577fb3b07489833bbf
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2022-10-02 19:46:01 -0500

    More recursion

--- Assets/scripts/Recursion/Recursion.cs
+++ Assets/scripts/Recursion/Recursion.cs
@@ -1,20 +1,45 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Recursion : MonoBehaviour
 {
-    public int factorialOfNumber;
+    [Header ("Recursion Dependencies")]
+    public int factorial;
+    public int fibbonacci;
+    public int[] array;
+    public List<float> list;
 
     public void Button()
     {
-        Debug.Log(Factorial(factorialOfNumber));
+        Debug.Log("Factorial de " + factorial + ": " + Factorial(factorial));
+        Debug.Log("Fibbonacci de " + fibbonacci + ": " + Fibbonacci(fibbonacci));
+        Debug.Log("El array contiene: " + ArrayCount(array) + " elementos");
+        Debug.Log("La suma del contenido de la lista es: " + SumOfTheList(list));
     }
 
     int Factorial(int n)
     {
-        if (n <= 1) return n;
-        n = n * Factorial(n-1);
-        return n;
+        if (n <= 1) return 1;
+        return n * (Factorial(n - 1));
+    }
+
+    int Fibbonacci(int n)
+    {
+        if(n <= 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        return Fibbonacci(n - 1) + Fibbonacci(n  - 2) + 1;
+    }
+
+    int ArrayCount(int[] n)
+    {
+        if (n.Length == 0) return 0;
+        return 1 + ArrayCount(n[1..]);
+    }
+
+    private float SumOfTheList(List<float> n)
+    {
+        if (n.Count == 0) return 0;
+        return n[0] + SumOfTheList(n.GetRange(1, n.Count - 1));
     }
 }
